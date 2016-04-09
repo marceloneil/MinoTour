21c1b1060977827726cda86095d032bfc5e2918b
splacorn <curtischong5@gmail.com>
2016-04-09 12:21:02 -0400

    google places api

--- app/src/main/java/com/minotour/minotour/MainApplication.java
+++ app/src/main/java/com/minotour/minotour/MainApplication.java
@@ -31,10 +31,9 @@ public class MainApplication extends Application {
         Flybits.include(this).initialize(builder);
 
 
-        GooglePlaces client = new GooglePlaces("");
+        GooglePlaces client = new GooglePlaces(BuildConfig.GoogleApiKey);
         List<Place> places = client.getNearbyPlaces(200, 500, 2000, GooglePlaces.MAXIMUM_RESULTS);
         for(Place p: places){
-
             System.out.println(p);
         }
 
