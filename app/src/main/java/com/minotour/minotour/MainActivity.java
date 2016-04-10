b960997241080c7a48beb72fe95b3ef4cc5cb753
Marcel O'Neil <marcel@marceloneil.com>
2016-04-09 23:52:59 -0400

    OkHTTP

--- app/src/main/java/com/minotour/minotour/MainActivity.java
+++ app/src/main/java/com/minotour/minotour/MainActivity.java
@@ -1,12 +1,15 @@
 package com.minotour.minotour;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -35,46 +38,52 @@ public class MainActivity extends Activity implements LocationListener{
         // Get the application instance
         app = (MainApplication) getApplication();
 
-
-
-
-        // Get the location manager
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // Define the criteria how to select the locatioin provider -> use
-        // default
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, true);
-        //provider = "gps";
-        //ActivityCompat.requestPermissions(this,
-          //      new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-            //    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        // Initialize the location fields
-        if (location != null) {
-            System.out.println("Provider " + provider + " has been selected.");
-            onLocationChanged(location);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         } else {
+            // Get the location manager
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            // Define the criteria how to select the location provider -> use
+            // default
+            Criteria criteria = new Criteria();
+            provider = locationManager.getBestProvider(criteria, true);
+            Location location = locationManager.getLastKnownLocation(provider);
 
-            System.out.print("Location not available");
+            // Initialize the location fields
+            if (location != null) {
+                System.out.println("Provider " + provider + " has been selected.");
+                onLocationChanged(location);
+            } else {
+                System.out.print("Location not available");
+            }
         }
-
-
-
     }
 
     /* Request updates at startup */
     @Override
     protected void onResume() throws SecurityException {
         super.onResume();
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
     }
 
     /* Remove the locationlistener updates when Activity is paused */
     @Override
     protected void onPause() throws SecurityException {
         super.onPause();
-        locationManager.removeUpdates(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 3);
+        } else {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.removeUpdates(this);
+        }
     }
 
     @Override
@@ -82,7 +91,7 @@ public class MainActivity extends Activity implements LocationListener{
         lat = (location.getLatitude());
         lng = (location.getLongitude());
         System.out.println("Latitude: " + lat + "     Longitude: " + lng);
-        ArrayList<Object> array = new ArrayList<Object>(Arrays.asList(lat,lng, 200, GooglePlaces.MAXIMUM_RESULTS));
+        ArrayList<Object> array = new ArrayList<Object>(Arrays.asList(lat,lng, 1000, GooglePlaces.MAXIMUM_RESULTS, "Food"));
         RetrieveNearbyPlaces get = new RetrieveNearbyPlaces();
         get.execute(array);
     }
@@ -104,5 +113,36 @@ public class MainActivity extends Activity implements LocationListener{
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Disabled provider " + provider,
                 Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) throws SecurityException {
+        if (requestCode == 1) {
+            if(grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Get the location manager
+                LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                // Define the criteria how to select the location provider -> use
+                // default
+                Criteria criteria = new Criteria();
+                provider = locationManager.getBestProvider(criteria, true);
+                Location location = locationManager.getLastKnownLocation(provider);
+
+                // Initialize the location fields
+                if (location != null) {
+                    System.out.println("Provider " + provider + " has been selected.");
+                    onLocationChanged(location);
+                } else {
+                    System.out.print("Location not available");
+                }
+            } else {
+                System.out.print("Location not available");
+            }
+        } else if (requestCode == 2){
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        } else if (requestCode == 3){
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.removeUpdates(this);
+        }
     }
 }
--- app/src/main/java/com/minotour/minotour/RetrieveNearbyPlaces.java
+++ app/src/main/java/com/minotour/minotour/RetrieveNearbyPlaces.java
@@ -14,9 +14,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Place;
-
 /**
  * Created by Marcel O'Neil on 09/04/16.
  * Using the google api
@@ -24,39 +21,62 @@ import se.walkercrou.places.Place;
 public class RetrieveNearbyPlaces extends AsyncTask<ArrayList, Void, List> {
 
     protected ArrayList doInBackground(ArrayList... arrayLists) {
-        GooglePlaces client = new GooglePlaces(BuildConfig.GoogleApiKey);
-        List<Place> places = client.getNearbyPlaces((double) arrayLists[0].get(0),(double) arrayLists[0].get(1),(int) arrayLists[0].get(2),(int) arrayLists[0].get(3));
-        for (Place p : places) {
-            Log.i("Locations", p.getName());
             try {
-                Map<String, Object> params = new LinkedHashMap<>();
-                params.put("key", BuildConfig.GoogleApiKey);
-                params.put("origins", (arrayLists[0].get(0)).toString() + "," + (arrayLists[0].get(1)).toString());
-                params.put("destinations", p.getLatitude() + "," + p.getLongitude());
+                OkHttpClient client = new OkHttpClient();
 
-                StringBuilder postData = new StringBuilder();
-                for (Map.Entry<String, Object> param : params.entrySet()) {
-                    if (postData.length() != 0) postData.append('&');
-                    postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-                    postData.append('=');
-                    postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                Map<String, Object> nearParams = new LinkedHashMap<>();
+                nearParams.put("key", BuildConfig.GoogleApiKey);
+                nearParams.put("location", arrayLists[0].get(0).toString() + "," + arrayLists[0].get(1).toString());
+                nearParams.put("radius", arrayLists[0].get(2).toString());
+                nearParams.put("keyword", arrayLists[0].get(4).toString());
+                nearParams.put("opennow", "true");
+
+                StringBuilder nearData = new StringBuilder();
+                for (Map.Entry<String, Object> param : nearParams.entrySet()) {
+                    if (nearData.length() != 0) nearData.append('&');
+                    nearData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                    nearData.append('=');
+                    nearData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
                 }
-                postData.insert(0, "json?");
+                nearData.insert(0, "json?");
 
-                OkHttpClient HTTPclient = new OkHttpClient();
-
-                Request request = new Request.Builder()
-                        .url("https://maps.googleapis.com/maps/api/distancematrix/" + postData)
+                Request nearRequest = new Request.Builder()
+                        .url("https://maps.googleapis.com/maps/api/place/nearbysearch/" + nearData)
                         .build();
-                Response response = HTTPclient.newCall(request).execute();
+                Response nearResponse = client.newCall(nearRequest).execute();
 
-                Log.i("Output:", response.body().string());
+                Log.i("link", "https://maps.googleapis.com/maps/api/place/nearbysearch/" + nearData);
+                Log.i("Response", nearResponse.body().string());
+
+                /*Map<String, Object> distParams = new LinkedHashMap<>();
+                distParams.put("key", BuildConfig.GoogleApiKey);
+                distParams.put("origins", (arrayLists[0].get(0)).toString() + "," + (arrayLists[0].get(1)).toString());
+                distParams.put("destinations", p.getLatitude() + "," + p.getLongitude());
+
+                StringBuilder distData = new StringBuilder();
+                for (Map.Entry<String, Object> param : distParams.entrySet()) {
+                    if (distData.length() != 0) distData.append('&');
+                    distData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                    distData.append('=');
+                    distData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+                }
+                distData.insert(0, "json?");
+
+                Request distRequest = new Request.Builder()
+                        .url("https://maps.googleapis.com/maps/api/distancematrix/" + distData)
+                        .build();
+                Response distResponse = client.newCall(distRequest).execute();*/
+
+                //Request request = new Request.Builder()
+                //        .url("https://maps.googleapis.com/maps/api/distancematrix/" + postData)
+               //         .build();
+               // Response response = HTTPclient.newCall(request).execute();
+
+                //Log.i("Output:", response.body().string());
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-        }
         return null;
     }
-
 }
